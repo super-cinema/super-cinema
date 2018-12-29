3e93d2525a53b5adf9e96db3099bbd91a0069dfb
Agnieszka Marszałek <agnieszka.marszalek07@gmail.com>
2018-12-29 11:07:37 +0100

    [frontend] display  movie types

--- frontend/src/app/edit-movie/edit-movie.component.ts
+++ frontend/src/app/edit-movie/edit-movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-movie',
@@ -33,24 +34,37 @@ export class EditMovieComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.movie = new Movie;
       this.movie.id = params['id'];
-      this.httpClient.get("http://localhost:8080/movie?movieId=" + this.movie.id)
-        .subscribe(
-          (data: any) => {
-            console.log(data);
-            this.movie.title = data.title;
-            this.movie.duration = data.duration;
-            this.movie.productionCountry = data.productionCountry;
-            this.movie.productionYear = data.productionYear;
-            this.movie.cast = data.cast;
-            this.movie.directors = data.cast;
-            this.movie.movieShow = data.movieShow;
-            this.movieTypes = data.movieTypes;
+        }
+      )
+    this.httpClient.get("http://localhost:8080/movie?movieId=" + this.movie.id)
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          this.movie.title = data.title;
+          this.movie.duration = data.duration;
+          this.movie.productionCountry = data.productionCountry;
+          this.movie.productionYear = data.productionYear;
+          this.movie.cast = data.cast;
+          this.movie.directors = data.cast;
+          this.movie.movieShow = data.movieShow;
+          this.movie.types = data.types;
+        })
 
-      })
 
+  }
 
+  wasTypeSelected(movieType: { value: string; name: string; checked: boolean }) {
+    if(this.movie.types.includes(movieType.value)) {
+      movieType.checked = true;
+      return true;
+    }
+    movieType.checked = false;
+    return false;
+  }
 
-  })}
+  checkMovieType(movieType, event) {
+    movieType.checked = !movieType.checked
+  }
 
 
 }
