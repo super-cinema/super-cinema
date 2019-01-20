fef9e7973d732a31e6cfe8e2f0262711af8a96d1
Krzysztof Pawski <pawski.krzysztof@gmail.com>
2019-01-20 11:58:53 +0100

    [ios] iOS app created from Tabbed App template

---
+++ frontend_ios/SuperCinema/SuperCinema/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SuperCinema
+//
+//  Created by Krzysztof Pawski on 20/01/2019.
+//  Copyright © 2019 SuperCinemaSpZoo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ frontend_ios/SuperCinema/SuperCinema/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SuperCinema
+//
+//  Created by Krzysztof Pawski on 20/01/2019.
+//  Copyright © 2019 SuperCinemaSpZoo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
