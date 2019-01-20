1d7100b38afbfd505805d80664fd3f15bbc4f073
Krzysztof Pawski <pawski.krzysztof@gmail.com>
2019-01-20 12:00:22 +0100

    [ios] Simple get request to verify if can connect to local backend

--- frontend_ios/SuperCinema/SuperCinema/FirstViewController.swift
+++ frontend_ios/SuperCinema/SuperCinema/FirstViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    var task: URLSessionTask?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let url = URL(string: "http://localhost:8080/movie")!
+        task = URLSession.shared.dataTask(with: url,
+                                          completionHandler: { (data, response, error) in
+                                            print("finished")
+                                            if let data = data {
+                                                let result = String(data: data, encoding: .utf8)
+                                                print(result)
+                                            }
+                                            if let error = error {
+                                                print(error)
+                                            }
+        })
+        task?.resume()
     }
 
 
