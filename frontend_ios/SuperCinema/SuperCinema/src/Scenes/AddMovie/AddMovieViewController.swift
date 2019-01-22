04091d185169ccc0bcd747602f271d764a087f31
Krzysztof Pawski <pawski.krzysztof@gmail.com>
2019-01-22 20:49:55 +0100

    [ios] Add year picker to AddMovieViewController

--- frontend_ios/SuperCinema/SuperCinema/src/Scenes/AddMovie/AddMovieViewController.swift
+++ frontend_ios/SuperCinema/SuperCinema/src/Scenes/AddMovie/AddMovieViewController.swift
@@ -5,6 +5,32 @@
 
 import UIKit
 
+final class PickerDataSource: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+    private weak var textField: UITextField?
+    private let values: [String]
+
+    init(values: [String], textField: UITextField?) {
+        self.values = values
+        self.textField = textField
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return values.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return values[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textField?.text = values[row]
+    }
+}
+
 final class AddMovieViewController: UIViewController {
 
     @IBOutlet weak private var titleTextField: UITextField!
@@ -13,6 +39,7 @@ final class AddMovieViewController: UIViewController {
     @IBOutlet weak private var countryTextField: UITextField!
 
     private var task: URLSessionTask?
+    private var yearPickerDataSource: PickerDataSource?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +49,39 @@ final class AddMovieViewController: UIViewController {
 
         let cancelMovieItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
         navigationItem.leftBarButtonItem = cancelMovieItem
+
+        let years = Array(1900...2019).map { String($0) }
+        yearPickerDataSource = PickerDataSource(values: years, textField: yearTextField)
+
+        let pickerView = UIPickerView()
+        pickerView.dataSource = yearPickerDataSource
+        pickerView.delegate = yearPickerDataSource
+        yearTextField.inputView = pickerView
+        pickerView.selectRow(100, inComponent: 0, animated: false)
+
+        let toolbar = toolbarPiker(mySelect: #selector(dismissPicker))
+        yearTextField.inputAccessoryView = toolbar
+
+    }
+
+    @objc private func dismissPicker() {
+        view.endEditing(true)
+    }
+
+    private func toolbarPiker(mySelect : Selector) -> UIToolbar {
+
+        let toolBar = UIToolbar()
+
+        toolBar.barStyle = .default
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: mySelect)
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        toolBar.setItems([ spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+
+        return toolBar
     }
 
     @objc private func addMovie() {
@@ -50,6 +110,5 @@ final class AddMovieViewController: UIViewController {
     @objc private func cancel() {
         dismiss(animated: true, completion: nil)
     }
-
 }
 
